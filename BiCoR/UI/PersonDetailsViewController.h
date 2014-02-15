b1215f73db900fab79bf14e5cd17c93f9c1f7a23
Markus Hinkelmann <markus@mhinkelmann.de>
2014-02-15 23:03:43 +0100

    - Added a TableView for the details

---
+++ BiCoR/UI/PersonDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonDetailsViewController.h
+//  BiCoR
+//
+//  Created by Markus Hinkelmann on 15.02.14.
+//  Copyright (c) 2014 Markus Hinkelmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonDetailsViewController : UITableViewController
+
+@end
